56744bbfdbed4e5ec7039ce038dbcf3254827359
Alexey <alexeymsa@gmail.com>
2018-10-08 18:38:27 +0300

    Сделал списание средств при регистрации на турнире + форма регистрации при клике вне области - закрывает popover

--- public/pluton/js/popup-form.js
+++ public/pluton/js/popup-form.js
@@ -66,9 +66,9 @@ function showErrorReg(error){
 
 
 $('body').on('click', function(e) {
-    $('[data-toggle=popover]').each(function() {
+    $('#auth-link').each(function() {
         // hide any open popovers when the anywhere else in the body is clicked
-        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('#uth-link').has(e.target).length === 0) {
             $(this).popover('hide');
         }
     });
