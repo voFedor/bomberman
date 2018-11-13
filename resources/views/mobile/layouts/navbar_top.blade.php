5c8d36bd05c806172df90466d4a577a292665f75
vofedor <mrfood06@gmail.com>
2018-11-13 15:46:56 +0300

    правки кнопок

--- resources/views/mobile/layouts/navbar_top.blade.php
+++ resources/views/mobile/layouts/navbar_top.blade.php
@@ -1,11 +1,13 @@
 <div class="navbar-top">
     <!-- site brand	 -->
     <div class="site-brand">
+        <a href="/" class="brand">
         <img src="/template/mobile/{{env('MOBILE_THEME')}}/img/logo.png"   width="134" height="40" alt="Logo" />
 
     </div>
     <!-- end site brand	 -->
     <div class="side-nav-panel-right">
-        <a href="#" data-activates="slide-out-right" class="side-nav-left">Логин<i class="fa fa-user-secret"></i></a>
+        <z href="#" data-activates="slide-out-right" class="side-nav-left"><i class="fa fa-user"></i>профиль</z>
+       <p></p>
     </div>
 </div>
