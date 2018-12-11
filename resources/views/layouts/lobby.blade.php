54949f91df8b15c35ef28431ec5eb17f4144e54b
vofedor <mrfood06@gmail.com>
2018-12-12 01:12:55 +0300

    правки платежки - проерить, чат вк

--- resources/views/layouts/lobby.blade.php
+++ resources/views/layouts/lobby.blade.php
@@ -34,14 +34,20 @@
 @if(Auth::check())
 <div id="mybutton">
 <button class="duel" onclick="return getDuel(1)">
-Назначить дуэль
+Решить спор БОМБЕРМЕНОМ
 </button>
 </div>
 
 <input type="hidden" id="game_id_for_vue" name="game_id_for_vue">
 <input type="hidden" id="bet_id_for_vue" name="bet_id_for_vue">
 @endif
+<script type="text/javascript" src="https://vk.com/js/api/openapi.js?160"></script>
 
+<!-- VK Widget -->
+<div id="vk_community_messages"></div>
+<script type="text/javascript">
+    VK.Widgets.CommunityMessages("vk_community_messages", 172832726, {expandTimeout: "30000",disableButtonTooltip: "1"});
+</script>
 
 @include('lobby.parts.javascript')
 
