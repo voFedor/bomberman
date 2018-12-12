698e88820e9c3378b2eac0e1c9d1ff2112270e5b
vofedor <mrfood06@gmail.com>
2018-12-12 17:25:22 +0300

    правки для HTTPS

--- resources/views/lobby/parts/head.blade.php
+++ resources/views/lobby/parts/head.blade.php
@@ -26,7 +26,7 @@
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/{{ env('THEME') }}/images/apple-touch-icon-72.png">
 <link rel="apple-touch-icon-precomposed" href="/{{ env('THEME') }}/images/ico/apple-touch-icon-57.png">
 <link rel="shortcut icon" href="/{{ env('THEME') }}/images/ico/favicon.ico">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.css">
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.css">
 
 
 <!-- Yandex.Metrika counter -->
